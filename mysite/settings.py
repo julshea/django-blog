ef49fa06f24edb7f44ddbc44e5d8073a93db283b
Julia Shea <jts6mq@virginiaedu>
2020-08-17 15:39:41 -0400

    first functonal test complete

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8dhg0-^kl1a7)pz^8e#olzak9&c-ry&q5nymp2+-xog*vb%4w6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS =  ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
